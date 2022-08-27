04bcb055bc538a66fc5eee9464750e48a2c04e5d
eddjhae <edd.bausa123@gmail.com>
2022-08-27 18:06:29 +0800

    added navbar mobile

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,8 +4,8 @@ export default function AboutMe() {
   return (
     <footer className="h-[100vh] min-h-[930px] mt-40 pt-12 relative bg-purple-600 rounded-tl-[100px] text-white font-poppins md:h-[110vh] lg:h-[100vh]">
       <div className="grid justify-items-center lg:grid-cols-[1fr_2fr]">
-        <h1 className="text-2xl font-semibold mb-8 md:text-3xl lg:text-4xl">About me</h1>
-        <div className="text-base w-[85%] md:text-lg lg:text-2xl">
+        <h1 className="text-2xl font-semibold mb-8 md:text-2xl lg:text-3xl">About me</h1>
+        <div className="text-base w-[85%] md:text-lg lg:text-xl">
           <p>Hello! I enjoy designing and developing things that exist on the internet. I'm always looking for new opportunities to learn, grow and enhance my skill set, currently learning React and NodeJS</p>
           <br />
           <p>Graduated in Lyceum of The Philippines University (LPU) BS in Information Technology.</p>
@@ -27,7 +27,7 @@ export default function AboutMe() {
           <button className="text-lg border border-blue-500 rounded-lg mt-3 px-4 py-3 font-poppins font-medium xl:text-2xl text-blue-500">
             Get In Touch
           </button>
-          <div className="text-center text-gray-400">
+          <div className="text-center text-gray-400 mt-12">
           ☕ Designed & Developed by Edd Jhae Bausa
         </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,76 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [navbar, setNavbar] = useState(false);
+
   return (
-    <header className="my-8 bg-white flex justify-between items-center">
-      <a href="#">
-        <img src="/assets/EJ-logo.svg" alt="logo" className="w-20" />
-      </a>
-      <nav>
-        <ul
-          className="mr-8 flex gap-14 font-semibold
-         text-blue-500 text-xl uppercase cursor-pointer">
-          <li>Projects</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Resume</li>
-        </ul>
+    <header className="w-full bg-white shadow ">
+      <nav className="justify-between w-full md:items-center md:flex">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="javascript:void(0)">
+              <img className="h-16" src="assets/EJ-logo.svg" alt="logo" />
+            </a>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">Projects</a>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">About me</a>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">Contact</a>
+              </li>
+              <li className="text-gray-600 hover:text-blue-600">
+                <a href="javascript:void(0)">Resume</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
